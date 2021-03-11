f76c664d3174b31a0e38376c9e6154f205d00e4f
Pooja Ingale <poojaingale0394@gmail.com>
2021-03-11 15:09:38 +0530

    Signed-off-by: Pooja Ingale <poojaingale0394@gmail.com>

--- SpringBoot_JDBC-1/src/main/java/com/app/SpringBootJdbc1Application.java
+++ SpringBoot_JDBC-1/src/main/java/com/app/SpringBootJdbc1Application.java
@@ -10,6 +10,7 @@ public class SpringBootJdbc1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJdbc1Application.class, args);
+		System.out.println("cloninig this project");
 	}
 
 }
